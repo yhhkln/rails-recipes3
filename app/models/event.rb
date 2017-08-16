abbc8101ac0693580fd7ffdf982c3402ed2275b5
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-16 21:11:32 +0800

    friendly

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,13 @@ class Event < ApplicationRecord
  validates_presence_of :name
 
  def to_param
-   "#{self.id}-#{self.name}"
+   self.friendly_id
+ end
+
+ protected
+
+ def generate_friendly_id
+   self.friendly_id ||= SecureRandom.uuid
  end
 
 end
