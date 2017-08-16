fc5e88764295f9a2fb0964a56e03aadb3dd2f671
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-16 23:27:34 +0800

    own-id

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,7 @@ class Event < ApplicationRecord
  validates_uniqueness_of :friendly_id
  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
 
+  
  before_validation :generate_friendly_id, :on => :create
 
  def to_param
